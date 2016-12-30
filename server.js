7831bf8ff301b3526e67cb2751acf05affaec75b
Mohammed <mohd.romi47@gmail.com>
2016-12-30 14:41:27 +0200

    installing mongodb

--- server.js
+++ server.js
@@ -9,7 +9,22 @@ const MongoClient = require('mongodb').MongoClient;
 	app.use(bodyParser.json())
 	app.use(express.static('public'))
 	var db;
-    
+    MongoClient.connect('mongodb://facebook-task:M0hdr0me@ds149268.mlab.com:49268/facebook-task', function(err, database) {
+		db=database;
+		if(!err) {
+			console.log("We are connected");
+		}else{
+			console.log(err);
+			console.log("We are not connected");
+		}
+		app.listen(5000, function() {
+      console.log('listening on 5000')
+    })
+    })
+	/*app.get('/', function(req, res) {
+      res.send('Hello World')
+    })*/
+	
 	app.get("/index2",function(req,res){
 		
 		res.sendFile(__dirname + "/index2.html");
@@ -17,9 +32,13 @@ const MongoClient = require('mongodb').MongoClient;
 	
 	
 	app.get('/', function(req, res){
-		res.render("index");
+		//res.sendFile(__dirname + '/index2.html');
+		db.collection('users').find().toArray(function(err, result) {
+		if (err) return console.log(err)
+		//console.log(result);
+		res.render('index.ejs', {users: result})
+		})
 	})
 	
-	app.listen(5000, function(){
-		console.log("app running");
-	})
+
+	
